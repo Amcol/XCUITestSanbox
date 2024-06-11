968c60169ef040e89772766b02ea6e0111a96400
Oleksii Petruk <amcol@macpaw.com>
2024-06-11 16:57:09 +0300

    Initial Commit

---
+++ XCUITestSandbox/XCUITestSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCUITestSandboxApp.swift
+//  XCUITestSandbox
+//
+//  Created by Amcol on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct XCUITestSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
