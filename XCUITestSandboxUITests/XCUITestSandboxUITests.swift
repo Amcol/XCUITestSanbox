b839f24cec7162b5e00db889d36269b5222d2bd0
Oleksii Petruk <amcol@macpaw.com>
2024-06-12 18:10:11 +0300

    first commit

--- XCUITestSandboxUITests/XCUITestSandboxUITests.swift
+++ XCUITestSandboxUITests/XCUITestSandboxUITests.swift
@@ -10,32 +10,35 @@ import XCTest
 final class XCUITestSandboxUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    override func tearDownWithError() throws {
+    }
+
+    func testToggle() throws {
+        
+        //GIVEN
+        let app = XCUIApplication()
+        let toggle = app.checkBoxes["Toggle"]
+        
+        //WHEN
+        toggle.click()
+        
+        //THEN
+        XCTAssertTrue(toggle.label.contains("Toggle State: On"), "Toggle state is not On")
+    }
+    
+    func testShowTextButton() throws {
+        let app = XCUIApplication()
+        let showTestButton = app.buttons["ShowTextButton"]
+        let helloWorldText = app.staticTexts["HelloWorldText"]
+        
+        showTestButton.click()
+        
+        XCTAssertTrue(helloWorldText.exists, "Text didn't appear")
     }
 }
